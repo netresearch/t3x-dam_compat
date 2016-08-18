3af366e0dfce1fe40377cb08f045dda2b2a25a44
Axel Kummer <axel.kummer@netresearch.de>
2016-08-18 15:15:15 +0200

    Add ext_tables.sql to prevent removing the '_migrated*' fields from sys_file and sys_catigory by installtool

---
+++ ext_tables.sql
@@ -0,0 +1,15 @@
+#
+# Table structure for table 'sys_file'
+#
+CREATE TABLE sys_file (
+    _migrateddamuid int(11) unsigned DEFAULT '0' NOT NULL
+    KEY migratedRecords (_migrateddamuid)
+);
+
+#
+# Table structure for table 'sys_category'
+#
+CREATE TABLE sys_category (
+	_migrateddamcatuid int(11) unsigned DEFAULT '0' NOT NULL
+    KEY migratedRecords (_migrateddamcatuid)
+);
